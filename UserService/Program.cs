c6787425e63581d7c1210dca825193b92fe72d1f
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-08-17 20:49:28 +0300

    JWT added

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,6 +1,9 @@
 
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using UserService.Data;
 
 namespace UserService
@@ -17,15 +20,31 @@ namespace UserService
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] 
+                                                                    ?? throw new NullReferenceException("Key can't be Null")))
+                    };
+                });
 
             builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
                         .ConfigureContainer<ContainerBuilder>(cb =>
                         {
                             var connectionString = builder.Configuration
                                                           .GetConnectionString("db")
-                                                          ?? throw new NullReferenceException("Connection String can't be Null");
+                                                          ?? throw new NullReferenceException("Connection string can't be Null");
                             cb.Register(c => new UserServiceContext(connectionString))
                               .InstancePerDependency();
+                        
                         });
 
             var app = builder.Build();
@@ -39,6 +58,7 @@ namespace UserService
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
