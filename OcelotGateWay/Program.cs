9abc1e2388e1917230835628225b98af595e3612
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-08-21 00:14:47 +0300

    ocelot added

---
+++ OcelotGateWay/Program.cs
@@ -0,0 +1,40 @@
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotGateWay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                                          .AddJsonFile("ocelot.json")
+                                          .Build();
+            builder.Services.AddOcelot(configuration);
+            builder.Services.AddSwaggerForOcelot(configuration);
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            }).UseOcelot().Wait();
+
+            app.UseHttpsRedirection();
+
+            app.Run();
+        }
+    }
+}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
-using UserService.AuthorizationModel;
 using UserService.Data;
 using UserService.Mapper;
 using UserService.Repo;
